c2be6458f3b2891afaebc627f0d00606f2e96514
Vishal Ludhrani <vludrani@gmail.com>
2022-03-02 19:13:31 +0530

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from "@mui/material";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Typography variant="h5" component="h3">Homejam Assignment</Typography>
+    </div>
+  );
+}
+
+export default App;
